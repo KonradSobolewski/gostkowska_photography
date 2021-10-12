081da5497d400180d8913dbd98ab82cbd940c333
Konrad Sobolewski <konradsob12@gmail.com>
2021-10-12 19:13:15 +0200

    test

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import '../style/App.css';
-import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav>
-      <li>
-        <Link to="/">Dogs</Link>
-      </li>
-      <li>
-        <Link to="/cats">Cats</Link>
-      </li>
-      <li>
-        <Link to="/sheeps">Sheeps</Link>
-      </li>
-      <li>
-        <Link to="/goats">Goats</Link>
-      </li>
+        <li>
+         O mnie
+        </li>
+        <li>
+         Galeria
+        </li>
+        <li>
+         Kontakt
+        </li>
     </nav>
   )
 
