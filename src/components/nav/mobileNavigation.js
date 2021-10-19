fb0f58f69dfeb8153d82d4ca4400215898a64c76
Konrad Sobolewski <konradsob12@gmail.com>
2021-10-19 22:34:24 +0200

    mobile nav

---
+++ src/components/nav/mobileNavigation.js
@@ -0,0 +1,202 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import logo1 from "../../assets/LOGO.png";
+import IG from "../../assets/ig.png";
+import FB from "../../assets/fb.png";
+
+const Background = styled.div`
+  color: #555;
+  background-color: rgba(250, 250, 250, 1);
+  transition: color 0.1s, background-color 0.5s ease-in-out;
+`;
+
+const Mobile = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+`;
+
+const FirstRow = styled(Background)`
+  z-index: 10;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginContainer = styled.div`
+  flex: 1;
+`;
+
+const Tab = styled.li`
+  list-style: none;
+  cursor: pointer;
+  padding: 0.8em 0px;
+  white-space: nowrap;
+  transition: all 0.5s ease-in-out;
+`;
+
+const NavLogoTab = styled(Tab)`
+  flex: 1;
+  padding: ${(props) => (props.show ? "1em" : ".5em")};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Img = styled.img`
+  filter: drop-shadow(0px 0px 4px #fff);
+  width: ${(props) => (props.show ? "8em" : "5em")};
+  transition: all 0.5s ease-in-out;
+`;
+
+const Hamburger = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const Input = styled.input`
+  position: absolute;
+  transform: scale(2.5);
+  opacity: 0; /* hide this */
+  z-index: 20; /* and place it over the hamburger */
+  -webkit-touch-callout: none;
+
+  :checked ~ span {
+    opacity: 1;
+    transform: rotate(-45deg) translate(-1px, 3px);
+  }
+
+  :checked ~ span:nth-last-child(2) {
+    opacity: 0;
+    transform: rotate(0deg) scale(0.3, 0.3);
+  }
+  :checked ~ span:nth-last-child(3) {
+    transform: rotate(45deg) translate(-1px, -1px);
+  }
+`;
+
+const Span = styled.span`
+  display: block;
+  width: 1.5em;
+  height: 3px;
+  margin: 2px;
+  position: relative;
+
+  background: #666;
+  z-index: 15;
+  transform-origin: 4px 1px;
+
+  transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
+    background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+
+  :first-child {
+    transform-origin: 0% 0%;
+  }
+
+  :nth-last-child(1) {
+    transform-origin: 0% 100%;
+  }
+`;
+
+const NavTabs = styled(Background)`
+  width: 100%;
+  z-index: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  transform: translateY(${(props) => (props.show ? "0%" : "-100%")});
+  transition: all 0.5s ease-in-out;
+`;
+
+const SocialMedia = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SocialMediaItem = styled.img`
+  padding: 2em;
+  width: 2em;
+  filter: invert(0.3);
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+`;
+
+// https://codepen.io/erikterwan/pen/EVzeRP
+const MobileNavigation = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClick = (event) => {
+    setShow(event.target.checked);
+  };
+
+  return (
+    <Mobile show={show}>
+      <FirstRow show={show}>
+        <LoginContainer></LoginContainer>
+        <NavLogoTab show={show}>
+          <a href=".">
+            <Img
+              show={show}
+              src={logo1}
+              id="gostkowska_logo"
+              alt={"Logo of Martyna Gostkowska"}
+            />
+          </a>
+        </NavLogoTab>
+        <Hamburger>
+          <Input type="checkbox" onClick={handleClick} />
+          <Span></Span>
+          <Span></Span>
+          <Span></Span>
+        </Hamburger>
+      </FirstRow>
+      <NavTabs show={show}>
+        <Tab show={show}>O mnie</Tab>
+        <Tab show={show}>Portfolio</Tab>
+        <Tab show={show}>Oferta</Tab>
+        <Tab show={show}>Kontakt</Tab>
+        <SocialMedia>
+          <a
+            href="https://www.instagram.com/gostkowska_martyna/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SocialMediaItem
+              show={show}
+              src={IG}
+              id="instagram"
+              alt={"Instagram logo"}
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/gostkowska.martyna"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SocialMediaItem
+              show={show}
+              src={FB}
+              id="facebook"
+              alt={"Facebook logo"}
+            />
+          </a>
+        </SocialMedia>
+      </NavTabs>
+    </Mobile>
+  );
+};
+
+export default MobileNavigation;
