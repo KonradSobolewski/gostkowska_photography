2221b4d5f107ffb4a8b98827ba2024de5ad2fe14
Konrad Sobolewski <konradsob12@gmail.com>
2022-02-07 19:07:10 +0100

    cookies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import NavBar from './components/nav/navBar';
-import Content from './components/content';
-import Contact from './components/contact/contact';
-import Footer from './components/footer/footer';
-import Offers from './components/offers/offers';
-import Portfolio from './components/portfolio/portfolio';
+import NavBar from "./components/nav/navBar";
+import Content from "./components/content";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
+import Offers from "./components/offers/offers";
+import Portfolio from "./components/portfolio/portfolio";
+import Cookies from "./components/cookies/cookies";
 
 function App() {
 
   return (
     <div>
+      <Cookies />
       <NavBar />
       <Content />
-      <Portfolio/>
-      <Offers/>
+      <Portfolio />
+      <Offers />
       <Contact />
       <Footer />
     </div>
